7abccde547e01df77b437782780540874789e718
shlevanto <simo.levanto@helsinki.fi>
2021-09-07 21:54:45 +0300

    added secret key for session management

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, session
+from werkzeug.security import generate_password_hash, check_password_hash
+from os import getenv
 
 app = Flask(__name__)
+
+app.secret_key = getenv('SECRET_KEY')
 
 @app.route('/')
 def index():
@@ -8,4 +12,9 @@ def index():
 
 @app.route('/login')
 def login():
+    # if a user is logged in, we redirect to index
+    try:
+        current_user = session['user']
+    except(KeyError):
+        return render_template('login.html')
     return ''
