b760b87a4f6ccc1c549d8afe09886c9817fe4c0b
shlevanto <simo.levanto@helsinki.fi>
2021-09-17 21:09:01 +0300

    Will os.environ make Heroku accessible?

--- db.py
+++ db.py
@@ -5,6 +5,7 @@ from os import getenv, environ
 # database configurations
 # dev
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
+environ.get('DATABASE_URL')
 
 db = SQLAlchemy(app)
 
