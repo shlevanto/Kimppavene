2997437df286ae92bc63930d191fdff796d2ccb4
shlevanto <simo.levanto@helsinki.fi>
2021-09-17 20:57:54 +0300

    heroku config

--- db.py
+++ db.py
@@ -1,9 +1,14 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
-from os import getenv
+from os import getenv, environ
 
 # database configurations
-app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
+
+# development
+# app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ohyvnilgnbwrpa:10fdd44d77a8fed845a712f37a4a8b9aa46068b147e1bf36a7d5ca2ea3f7359e@ec2-44-198-151-32.compute-1.amazonaws.com:5432/d3utq4fl0ipuuj'
+
 db = SQLAlchemy(app)
 
 # get rid of error message when starting server
