c43f9038ab305831c11dbb5d2b32372163ff2ad8
shlevanto <simo.levanto@helsinki.fi>
2021-10-04 22:44:55 +0300

    changed login structure to use guard clauses
    changed method for redirecting user based on amount of boats in database (if 0 or > 1 -> redirect to boats with appropriate message) else redirect to index

--- views/login.py
+++ views/login.py
@@ -29,49 +29,65 @@ def loginuser_view():
     if not user:
         flash('Virheellinen käyttäjätunnus tai salasana.')
         return redirect('/login')
-    else:
-        hash_value = user.password
-        if check_password_hash(hash_value, password):
-            session['user'] = {
-                'username': user.username,
-                'id': user.id,
-                'first_name': user.first_name,
-                'last_name': user.last_name
-                }
-            session['csrf_token'] = token_hex(16)
 
-            sql = '''
-                SELECT name, id 
-                    FROM boats WHERE id = (
-                        SELECT boat_id FROM owners WHERE user_id=:user_id LIMIT 1
-                        )
-                '''
-            result = db.session.execute(sql, {'user_id': user.id})
-            # this now selects the first boat of the owner
-            # if 0 --> redirect to create / join boat
-            # if > 1 --> redirect to choose boat
-            # this needs to be rethought...
-            boat = result.fetchone()
+    hash_value = user.password
 
-            if boat:
-                session['boat'] = {
-                    'id': boat.id,
-                    'name': boat.name
-                }
-                return redirect('/')
-            else:
-                session['boat'] = {
-                    'id': '',
-                    'name': ''}
-                flash(
-                    '''
-                        Sinulla ei ole vielä veneitä järjestelmässä. 
-                        Luo uusi vene tai liity olemassaolevaan veneeseen avainkoodilla.
-                    '''
-                )
+    if not check_password_hash(hash_value, password):
+        flash('Virheellinen käyttäjätunnus tai salasana.')
+        return redirect('/login')
 
-                return redirect('/boats')
-        else:
-            flash('Virheellinen käyttäjätunnus tai salasana.')
-            return redirect('/login')
+    # if the login is successfull, set user as session['user']
+    session['user'] = {
+        'username': user.username,
+        'id': user.id,
+        'first_name': user.first_name,
+        'last_name': user.last_name
+        }
+    session['csrf_token'] = token_hex(16)
 
+    # get list of user's boats
+    sql = '''
+        SELECT boats.name, boats.id 
+            FROM boats 
+            JOIN owners ON boats.id=owners.boat_id 
+            WHERE owners.user_id=:user_id;
+        '''
+    result = db.session.execute(sql, {'user_id': user.id})
+    boats = result.fetchall()
+
+    # if no boats, redirect to create / join boat
+    if not boats:
+        session['boat'] = {
+            'id': '',
+            'name': ''}
+        flash(
+            '''
+                Sinulla ei ole vielä veneitä järjestelmässä. 
+                Luo uusi vene tai liity olemassaolevaan veneeseen avainkoodilla.
+            '''
+        )
+
+        return redirect('/boats')
+
+    # if many boats, redirect to choose boat
+    if len(boats) > 1:
+        session['boat'] = {
+            'id': '',
+            'name': ''}
+
+        flash(
+            '''
+                Sinulla on useita veneitä järjestelmässä. 
+                Valitse mitä venettä haluat tarkastella.
+            '''
+        )
+
+        return redirect('/boats')
+
+    # if one boat, login and redirect to index
+    session['boat'] = {
+        'id': boats[0].id,
+        'name': boats[0].name
+    }
+
+    return redirect('/')
